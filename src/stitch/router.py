b768fb145ed2250629adecfd61358bb2acbf06ea
fbraza <fbraza@tutanota.com>
2025-08-13 18:30:14 +0200

    feat: error handling duplicated proc. mutations implemented

--- src/stitch/router.py
+++ src/stitch/router.py
@@ -5,17 +5,47 @@ from typing import Any, get_type_hints
 from stitch import extractor
 
 
+class BaseRouterError(Exception):
+    """Base error class for Etherscan errors"""
+
+
+class DuplicateProcedureError(BaseRouterError):
+    def __init__(self, proc: dict[str, Any], proc_name: str):
+        msg: str = f"""
+        message : Duplicate procedure name\n
+        procedure: {proc_name} already exists\n
+        expected: not in {proc.keys()}
+        """
+        super(BaseRouterError, self).__init__(msg)
+
+
 class Router:
     def __init__(self):
-        self.proc = {}
+        self.proc: dict[str, Any] = {}
 
     def get_schema(self) -> dict[str, Any]:
-        return {
-            name: {"type": proc["type"], "schema": proc["schema"]}
-            for name, proc in self.proc.items()
-        }
+        if self.proc:
+            return {
+                name: {"schema": proc["schema"]} for name, proc in self.proc.items()
+            }
+
+        return self.proc
 
     def query(self, name: str | None = None) -> Callable[[Callable], Callable]:
+        """
+        Decorator that registers a function as a query handler.
+        """
+        return self.__make_decorator(type="query", name=name)
+
+    def mutation(self, name: str | None = None) -> Callable[[Callable], Callable]:
+        """
+        Decorator that registers a function as a mutation handler.
+        """
+        return self.__make_decorator(type="mutation", name=name)
+
+    def __make_decorator(
+        self, type: str, name: str | None = None
+    ) -> Callable[[Callable], Callable]:
         """
         Minimal query decorator that:
         1. Registers the function
@@ -23,15 +53,18 @@ class Router:
         3. Stores schema for client consumption
         """
 
-        def decorator(func: Callable) -> Callable:
+        def __decorator(func: Callable) -> Callable:
             proc_name = name or func.__name__
+
+            if proc_name in self.proc.keys():
+                raise DuplicateProcedureError(proc=self.proc, proc_name=proc_name)
 
             # Extract type information
             type_hints = get_type_hints(func)
             sig = inspect.signature(func)
 
             self.proc[proc_name] = {
-                "type": "query",
+                "type": type,
                 "handler": func,
                 "signature": sig,
                 "type_hints": type_hints,
@@ -40,4 +73,4 @@ class Router:
 
             return func
 
-        return decorator
+        return __decorator
--- tests/test_router.py
+++ tests/test_router.py
@@ -1,6 +1,7 @@
+import pytest
 from pydantic import BaseModel
 
-from stitch.router import Router
+from stitch.router import DuplicateProcedureError, Router
 
 
 class User(BaseModel):
@@ -14,8 +15,8 @@ def test_router_query():
     @router.query()
     def get_user(user_id: int) -> User: ...  # type: ignore
 
-    assert router.proc["get_user"]["type"] == "query"
     assert router.proc["get_user"]["handler"] == get_user
+    assert router.proc["get_user"]["type"] == "query"
     assert router.proc["get_user"]["schema"] == {
         "input": {
             "properties": {"user_id": {"type": "integer"}},
@@ -32,3 +33,21 @@ def test_router_query():
             }
         },
     }
+
+
+def test_raise_for_duplciated_procs():
+    router = Router()
+
+    @router.query("get_user")
+    def get_user(user_id: int) -> User: ...  # type: ignore
+
+    with pytest.raises(DuplicateProcedureError, match="Duplicate procedure name"):
+
+        @router.query("get_user")
+        def collect_user(user_id: int) -> User: ...  # type: ignore
+
+
+def test_empty_router():
+    router = Router()
+
+    assert router.get_schema() == {}
